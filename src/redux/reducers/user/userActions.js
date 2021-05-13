e6022bc5214389a31118696e895a6bc3b13edac4
shanpig <shanpigliao@gmail.com>
2021-05-13 17:10:13 +0800

    [remove] addToFavorite action [add] addToKitchen action [add] removeFromKitchen action [add] setLeftOvers action [add] setLeftOver action

--- src/redux/reducers/user/userActions.js
+++ src/redux/reducers/user/userActions.js
@@ -1,9 +1,40 @@
-const addToFavorite = (id) => {
-  console.log('action created');
+const setUser = (user_info) => {
   return {
-    type: 'user/add/favorite',
+    type: 'user/set/all',
+    payload: user_info,
+  };
+};
+
+const setLeftOvers = (leftOvers) => {
+  console.log('set left over to: ', leftOvers);
+  return {
+    type: 'user/set/leftOvers',
+    payload: leftOvers,
+  };
+};
+
+const setLeftOver = (leftOver) => {
+  console.log('set one leftover: ', leftOver);
+  return {
+    type: 'user/set/leftOver',
+    payload: leftOver,
+  };
+};
+
+const addToKitchen = (id) => {
+  console.log('add to kitchen: ', id);
+  return {
+    type: 'user/add/kitchen',
     payload: id,
   };
 };
 
-export { addToFavorite };
+const removeFromKitchen = (id) => {
+  console.log('remove from kitchen: ', id);
+  return {
+    type: 'user/remove/kitchen',
+    payload: id,
+  };
+};
+
+export { setUser, setLeftOvers, setLeftOver, addToKitchen, removeFromKitchen };
--- src/redux/reducers/user/userReducer.js
+++ src/redux/reducers/user/userReducer.js
@@ -5,11 +5,40 @@ export default function userReducer(state = user_info, action) {
     case 'user/set/all': {
       return action.payload;
     }
-    case 'user/add/favorite': {
-      console.log(action.payload);
+
+    case 'user/set/leftOvers': {
       return {
         ...state,
-        my_favorites: [...state.my_favorites, action.payload],
+        left_overs: action.payload,
+      };
+    }
+
+    case 'user/set/leftOver': {
+      let newIngredient = action.payload;
+      let newLeftOvers = state.left_overs.map((left) => {
+        if (left.ingredient_name === newIngredient.ingredient_name) {
+          return newIngredient;
+        } else return left;
+      });
+
+      return {
+        ...state,
+        left_overs: newLeftOvers,
+      };
+    }
+
+    case 'user/add/kitchen': {
+      return {
+        ...state,
+        my_kitchen: [...state.my_kitchen, action.payload],
+      };
+    }
+
+    case 'user/remove/kitchen': {
+      const removeTargetId = action.payload;
+      return {
+        ...state,
+        my_kitchen: state.my_kitchen.filter((id) => id !== removeTargetId),
       };
     }
 
