b1f706fa261089974eb3af3a5982e2215d828fa0
shanpig <shanpigliao@gmail.com>
2021-05-14 11:39:06 +0800

    [remove] testing code

--- src/App.js
+++ src/App.js
@@ -1,14 +1,5 @@
 import './App.css';
 import { useSelector, useDispatch } from 'react-redux';
-import { setLeftOvers } from './redux/reducers/user/userActions';
-
-const left = [
-  {
-    ingredient_name: '蛋',
-    ingredient_amount: 55,
-    ingredient_unit: '粒',
-  },
-];
 
 function App() {
   const d = useDispatch();
@@ -17,9 +8,7 @@ function App() {
   return (
     <div>
       <h2>hello</h2>
-      <button onClick={() => d(setLeftOvers(left))}>
-        click to remove favorite
-      </button>
+
       <div>
         {search_keywords.map((keyword, i) => (
           <p key={i}>{Object.values(keyword).join(' ')}</p>
