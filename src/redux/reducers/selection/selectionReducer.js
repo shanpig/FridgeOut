bac093a653c557e0e4350eea46f363726b68697c
shanpig <shanpigliao@gmail.com>
2021-05-20 10:49:57 +0800

    [remove] redundant reducers and state

--- src/redux/reducers/selection/selectionReducer.js
+++ src/redux/reducers/selection/selectionReducer.js
@@ -1,7 +1,4 @@
-import {
-  selected_left_overs as leftovers,
-  selected_recipes as recipes,
-} from '../../../mockData/state/state.json';
+import { selected_recipes as recipes } from '../../../mockData/state/state.json';
 
 function recipesReducer(state = recipes, action) {
   let newSelectedRecipes = state.slice();
@@ -23,48 +20,4 @@ function recipesReducer(state = recipes, action) {
   }
 }
 
-function leftoversReducer(state = leftovers, action) {
-  let newSelectedLeftovers = state.slice();
-
-  switch (action.type) {
-    case 'select/add/leftover': {
-      const {
-        ingredient_name: name,
-        ingredient_amount: amount,
-        ingredient_unit: unit,
-      } = action.payload;
-      const indexOfIngredient = newSelectedLeftovers.findIndex(
-        (k) => k.ingredient_name === name && k.ingredient_unit === unit
-      );
-      if (indexOfIngredient < 0) {
-        newSelectedLeftovers.push(action.payload);
-      } else {
-        console.log(indexOfIngredient);
-        newSelectedLeftovers[indexOfIngredient].ingredient_amount += amount;
-      }
-
-      return newSelectedLeftovers;
-    }
-
-    case 'select/remove/leftover': {
-      const {
-        ingredient_name: name,
-        ingredient_amount: amount,
-        ingredient_unit: unit,
-      } = action.payload;
-      const indexOfIngredient = newSelectedLeftovers.findIndex(
-        (k) => k.ingredient_name === name
-      );
-      if (indexOfIngredient >= 0)
-        newSelectedLeftovers = [
-          ...newSelectedLeftovers.slice(0, indexOfIngredient),
-          ...newSelectedLeftovers.slice(indexOfIngredient + 1),
-        ];
-      return newSelectedLeftovers;
-    }
-    default:
-      return newSelectedLeftovers;
-  }
-}
-
-export { recipesReducer, leftoversReducer };
+export default recipesReducer;
