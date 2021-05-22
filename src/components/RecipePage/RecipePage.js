97c02ea44d7fd295b3c6bdf88eed64345c8b95d5
shanpig <shanpigliao@gmail.com>
2021-05-22 09:28:04 +0800

    [add] sidebarBody to recipePage

--- src/components/RecipePage/RecipePage.js
+++ src/components/RecipePage/RecipePage.js
@@ -1,9 +1,13 @@
 import React, { useEffect, useState } from 'react';
+import { theme } from '../../variables';
 import styled from 'styled-components';
-import { useParams } from 'react-router';
+import { useParams, useHistory } from 'react-router';
 import { getRecipe } from '../../utils/firebase';
+import SidebarBody from '../common/SidebarBody';
+import { BiArrowBack } from 'react-icons/bi';
 
 export default function RecipePage() {
+  const history = useHistory();
   let cat = '';
   let group = '';
   const [{ id, title, main_image, ingredients, steps }, setRecipe] = useState(
@@ -18,67 +22,78 @@ export default function RecipePage() {
   }, [id]);
 
   return (
-    <Container>
-      <RecipeImageContainer>
-        <RecipeImage src={main_image} alt='' />
-        <AddToButtonGroup>
-          <AddToButton key={1} value=''>
-            + 收藏
-          </AddToButton>
-          <AddToButton key={2} value=''>
-            + 我的廚房
-          </AddToButton>
-        </AddToButtonGroup>
-      </RecipeImageContainer>
-      <Title>{title}</Title>
-      <ListTitle>食材：</ListTitle>
+    <>
+      <Container src={main_image}>
+        <GoBackButton onClick={history.goBack}></GoBackButton>
+        <RecipeImageContainer>
+          <RecipeImage src={main_image} alt='' />
+          <AddToButtonGroup>
+            <AddToButton key={1} value=''>
+              + 收藏
+            </AddToButton>
+            <AddToButton key={2} value=''>
+              + 我的廚房
+            </AddToButton>
+          </AddToButtonGroup>
+        </RecipeImageContainer>
+        <Title>{title}</Title>
+        <ListTitle>食材：</ListTitle>
 
-      <IngredientList>
-        {ingredients &&
-          ingredients.map((ingredient, i) => {
-            let ingredientItem = [];
+        <IngredientList>
+          {ingredients &&
+            ingredients.map((ingredient, i) => {
+              let ingredientItem = [];
 
-            if (ingredient.ingredient_cat !== cat) {
+              if (ingredient.ingredient_cat !== cat) {
+                ingredientItem.push(
+                  <IngredientCat>{ingredient.ingredient_cat}</IngredientCat>
+                );
+                cat = ingredient.ingredient_cat;
+              }
+              if (ingredient.ingredient_group !== group) {
+                ingredientItem.push(
+                  <IngredientGroup>
+                    {ingredient.ingredient_group}
+                  </IngredientGroup>
+                );
+                group = ingredient.ingredient_group;
+              }
+
               ingredientItem.push(
-                <IngredientCat>{ingredient.ingredient_cat}</IngredientCat>
+                <Ingredient>
+                  {ingredient.ingredient_name}&nbsp;
+                  {ingredient.ingredient_amount}&nbsp;
+                  {ingredient.ingredient_unit}
+                </Ingredient>
               );
-              cat = ingredient.ingredient_cat;
-            }
-            if (ingredient.ingredient_group !== group) {
-              ingredientItem.push(
-                <IngredientGroup>{ingredient.ingredient_group}</IngredientGroup>
-              );
-              group = ingredient.ingredient_group;
-            }
 
-            ingredientItem.push(
-              <Ingredient>
-                {ingredient.ingredient_name}&nbsp;
-                {ingredient.ingredient_amount}&nbsp;
-                {ingredient.ingredient_unit}
-              </Ingredient>
-            );
+              return <IngredientItem key={i}>{ingredientItem}</IngredientItem>;
+            })}
+        </IngredientList>
 
-            return <IngredientItem key={i}>{ingredientItem}</IngredientItem>;
-          })}
-      </IngredientList>
-
-      <StepTitle>步驟：</StepTitle>
-      <Steps>
-        {steps &&
-          steps.map((step, i) => (
-            <Step key={i}>
-              <span>{i + 1}.</span>
-              <span>{step.trim()}</span>
-            </Step>
-          ))}
-      </Steps>
-    </Container>
+        <StepTitle>步驟：</StepTitle>
+        <Steps>
+          {steps &&
+            steps.map((step, i) => (
+              <Step key={i}>
+                <span>{i + 1}.</span>
+                <span>{step.trim()}</span>
+              </Step>
+            ))}
+        </Steps>
+      </Container>
+      <SidebarBody />
+    </>
   );
 }
 
 const Container = styled.div`
   padding: 45px 25px 5px;
+`;
+
+const GoBackButton = styled(BiArrowBack)`
+  cursor: pointer;
+  transform: scale(1.3);
 `;
 
 const RecipeImageContainer = styled.div`
