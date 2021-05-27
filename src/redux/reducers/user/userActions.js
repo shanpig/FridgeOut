78b5dfab750ca3bef134ed772816dc2b7587673d
shanpig <shanpigliao@gmail.com>
2021-05-27 16:24:48 +0800

    [add] thunk functions

--- src/redux/reducers/user/userActions.js
+++ src/redux/reducers/user/userActions.js
@@ -1,3 +1,11 @@
+import {
+  setUserLeftovers,
+  addRecipeToUserKitchen,
+  removeRecipeFromUserKitchen,
+  addRecipeToUserFavorites,
+  removeRecipeFromUserFavorites,
+} from '../../../utils/firebase';
+
 const setUser = (user_info) => {
   return {
     type: 'user/set/all',
@@ -5,7 +13,7 @@ const setUser = (user_info) => {
   };
 };
 
-const setLeftOvers = (leftOvers) => {
+const _setLeftOvers = (leftOvers) => {
   console.log('set left over to: ', leftOvers);
   return {
     type: 'user/set/leftOvers',
@@ -13,15 +21,16 @@ const setLeftOvers = (leftOvers) => {
   };
 };
 
-const setLeftOver = (leftOver) => {
-  console.log('set one leftover: ', leftOver);
-  return {
-    type: 'user/set/leftOver',
-    payload: leftOver,
+const setLeftOvers = (leftOvers) => {
+  return function (dispatch, getState) {
+    const userInfo = getState().user_info;
+    if (userInfo.identity !== 'none') setUserLeftovers(userInfo.id, leftOvers);
+
+    return dispatch(_setLeftOvers(leftOvers));
   };
 };
 
-const addToFavorite = (recipe) => {
+const _addToFavorite = (recipe) => {
   console.log('add to favorite: ', recipe);
   return {
     type: 'user/add/favorite',
@@ -29,18 +38,74 @@ const addToFavorite = (recipe) => {
   };
 };
 
-const removeFromFavorite = (recipe) => {
-  console.log('remove from favorite: ', recipe);
+const addToFavorite = (recipe) => {
+  return async function (dispatch, getState) {
+    const userInfo = getState().user_info;
+    if (userInfo.identity !== 'none') {
+      await addRecipeToUserFavorites(userInfo.id, recipe);
+    }
+    return dispatch(_addToFavorite(recipe));
+  };
+};
+
+const _removeFromFavorite = (id) => {
+  console.log('remove from favorite: ', id);
   return {
     type: 'user/remove/favorite',
+    payload: id,
+  };
+};
+
+const removeFromFavorite = (recipe) => {
+  return async function (dispatch, getState) {
+    const userInfo = getState().user_info;
+    if (userInfo.identity !== 'none')
+      await removeRecipeFromUserFavorites(userInfo.id, recipe);
+
+    return dispatch(_removeFromFavorite(recipe.id));
+  };
+};
+
+const _addToKitchen = (recipe) => {
+  return {
+    type: 'user/add/kitchen',
     payload: recipe,
+  };
+};
+
+const addToKitchen = (recipe) => {
+  return function (dispatch, getState) {
+    const userInfo = getState().user_info;
+    if (userInfo.identity !== 'none') {
+      addRecipeToUserKitchen(userInfo.id, recipe);
+    }
+    return dispatch(_addToKitchen(recipe));
+  };
+};
+
+const _removeFromKitchen = (id) => {
+  return {
+    type: 'user/remove/kitchen',
+    payload: id,
+  };
+};
+
+const removeFromKitchen = (recipe) => {
+  return async function (dispatch, getState) {
+    const userInfo = getState().user_info;
+    if (userInfo.identity !== 'none') {
+      await removeRecipeFromUserKitchen(userInfo.id, recipe);
+      console.log('hi, ', userInfo.name);
+    }
+    return dispatch(_removeFromKitchen(recipe.id));
   };
 };
 
 export {
   setUser,
   setLeftOvers,
-  setLeftOver,
   addToFavorite,
   removeFromFavorite,
+  addToKitchen,
+  removeFromKitchen,
 };
