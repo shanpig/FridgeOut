a597bbcbb79ad0808bbb85a712aa9403d3b6883f
shanpig <shanpigLiao@gmail.com>
2021-06-21 15:53:49 +0800

    [test] cicd

--- src/components/LandingPage/LandingPage.js
+++ src/components/LandingPage/LandingPage.js
@@ -13,7 +13,7 @@ export default function LandingPage() {
       <MainContent>
         <Column>
           <Animated animationIn="fadeInDown">
-            <H1>Fridge Out</H1>
+            <H1>Test CICD</H1>
           </Animated>
           <Animated animationIn="fadeInUp">
             <Text>尊重食材，用至分毫。</Text>
